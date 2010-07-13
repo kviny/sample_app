a5522a22909e48cfd435c98c35e592c9cfc3a5a9
kviny <kviny@ubuntu.(none)>
2010-07-13 14:40:36 +0400

    Finished layout and routes, but rspec don't work properly

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,11 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title
+    if @title.nil?
+      "Ruby on Rails Tutorial Sample App"
+    else
+      "Ruby on Rails Tutorial Sample App | #{@title}"
+    end
+  end
 end
+
