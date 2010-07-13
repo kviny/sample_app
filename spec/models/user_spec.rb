8b539e67234885f3f16c2321b1280a161cc09ebd
kviny <klyukvin@gmail.com>
2010-07-14 01:16:53 +0400

    added some stuff

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,4 +11,36 @@ describe User do
   it "should create a new instance given valid attributes" do
     User.create!(@valid_attributes)
   end
+  
+  it "should require a name" do 
+    noname_user = User.new(@valid_attributes.merge(:name => ""))
+    noname_user.should_not be_valid
+  end
+  
+  it "should require an email" do
+    nomail_user = User.new(@valid_attributes.merge(:email => ""))
+    nomail_user.should_not be_valid
+  end
+  
+  it "should reject names that are too long" do
+    long_name = "a" * 51
+    long_name_user = User.new(@valid_attributes.merge(:name => long_name))
+    long_name_user.should_not be_valid
+  end
+  
+  it "should accept valid email addresses" do
+    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses.each do |address|
+      valid_email_user = User.new(@valid_attributes.merge(:email => address))
+      valid_email_user.should be_valid
+    end
+  end
+  
+  it "should reject invalid email addresses" do
+    addresses = %w[user@foo,com user_at_foo.org example.user@foo.]
+    addresses.each do |address|
+      invalid_email_user = User.new(@valid_attributes.merge(:email => address))
+      invalid_email_user.should_not be_valid
+    end
+  end
 end
